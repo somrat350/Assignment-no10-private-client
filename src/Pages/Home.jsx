3f1b71c0d5565c589ad3740e442bac4117073e63
somrat350 <mdsomratsordaro350@gmail.com>
2025-11-09 15:25:57 +0600

    completed the hero slider degine using swiper js

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,9 @@
+import HeroSlider from "../Components/HeroSlider";
+
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className="max-w-7xl mx-auto p-5">
+      <HeroSlider/>
     </div>
   );
 };
