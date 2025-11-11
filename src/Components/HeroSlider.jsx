b29817e037ffa8baef150e8afa5f9bf30a51300c
somrat350 <mdsomratsordaro350@gmail.com>
2025-11-11 18:37:04 +0600

    dynamicaly fetch hero slider data from database

--- src/Components/HeroSlider.jsx
+++ src/Components/HeroSlider.jsx
@@ -1,55 +1,61 @@
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import { Autoplay, Navigation, Pagination } from 'swiper/modules';
-import { Link } from 'react-router';
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import { Link } from "react-router";
+import useAxios from "../Hooks/useAxios";
+import { useEffect, useState } from "react";
 
 const HeroSlider = () => {
+  const instance = useAxios();
+  const [sliders, setSliders] = useState([]);
+
+  useEffect(() => {
+    instance.get("/heroSlider").then((result) => {
+      setSliders(result.data);
+    });
+  }, [instance]);
   return (
     <Swiper
-        slidesPerView={1}
-        spaceBetween={30}
-        loop={true}
-        autoplay={{
-          delay: 1000,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Autoplay,Pagination, Navigation]}
-        className="w-full bg-[#101214]/20 rounded-lg p-5"
-      >
-        <SwiperSlide>
+      slidesPerView={1}
+      spaceBetween={30}
+      loop={true}
+      autoplay={{
+        delay: 3000,
+        disableOnInteraction: false,
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      navigation={true}
+      modules={[Autoplay, Pagination, Navigation]}
+      className="w-full bg-[#101214]/20 rounded-lg p-5"
+    >
+      {sliders.map((slider) => (
+        <SwiperSlide key={slider._id}>
           <div className="p-5 flex flex-col-reverse sm:flex-row gap-3 sm:gap-10 items-center justify-center">
-            <div className="flex flex-col gap-4 max-w-md">
-              <h2 className='text-3xl font-semibold'>Slider text </h2>
-              <p className='text-lg font-medium'>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, minus!</p>
-              <Link className='btn btn-primary'>Book Now</Link>
+            <div className="flex-1 flex flex-col gap-4 max-w-md">
+              <h2 className="text-3xl font-semibold">{slider.carName}</h2>
+              <p className="text-lg font-medium line-clamp-3">{slider.carDesc}</p>
+              <Link to={`/car/${slider._id}`} className="btn btn-primary">
+                Book Now
+              </Link>
             </div>
-            <div className="w-full max-w-sm flex items-center justify-center">
-              <img className='w-full rounded-2xl' src="https://images.unsplash.com/photo-1544636331-e26879cd4d9b?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80" alt="" />
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="p-5 flex flex-col-reverse sm:flex-row gap-10 items-center justify-center">
-            <div className="flex flex-col gap-4 max-w-md">
-              <h2 className='text-3xl font-semibold'>Slider text </h2>
-              <p className='text-lg font-medium'>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, minus!</p>
-              <Link className='btn btn-primary'>Book Now</Link>
-            </div>
-            <div className="w-full max-w-sm flex items-center justify-center">
-              <img className='w-full rounded-2xl' src="https://images.unsplash.com/photo-1544636331-e26879cd4d9b?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80" alt="" />
+            <div className="flex-1 w-full max-w-sm flex items-center justify-center">
+              <img
+                className="w-full rounded-2xl"
+                src={slider.carImageUrl}
+                alt={slider.carName}
+              />
             </div>
           </div>
         </SwiperSlide>
-      </Swiper>
+      ))}
+    </Swiper>
   );
 };
 
